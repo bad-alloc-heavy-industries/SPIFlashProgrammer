56d71b52f601f7c12f104bcbed7be87185d7ff5e
dx-mon <dx-mon@users.sourceforge.net>
2014-04-05 21:16:51 +0100

    Set the SPI0 pins to digital alternative-function

--- SPIFlash.c
+++ SPIFlash.c
@@ -33,8 +33,9 @@ int main()
 	GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
 	GPIO_PORTF_CR_R |= 0x01;
 	GPIO_PORTF_LOCK_R = 0;
-	/* Set them to digital mode */
+	/* Set the ports to digital mode */
 	GPIO_PORTA_DEN_R |= 0x3C;
+	GPIO_PORTF_DEN_R |= 0x1F;
 	/* Wait for SSI0 to come online */
 	while ((SYSCTL_PRSSI_R & SYSCTL_PRSSI_R0) != SYSCTL_PRSSI_R0);
 
@@ -47,11 +48,9 @@ int main()
 	/* Set the blue LED on */
 	GPIO_PORTF_DATA_BITS_R[0x0E] = 0x04;
 
-	//GPIO_PORTA_AFSEL_R;
-
+	GPIO_PORTA_AFSEL_R = 0x3C;
 	GPIO_PORTA_PCTL_R |= GPIO_PCTL_PA5_SSI0TX | GPIO_PCTL_PA4_SSI0RX |
 		GPIO_PCTL_PA3_SSI0FSS | GPIO_PCTL_PA2_SSI0CLK;
-	GPIO_PORTA_DEN_R;
 	/* Set Freescale SPI, SPO = 0, SPH = 1 */
 	SSI0_CR0_R = SSI_CR0_SPH | SSI_CR0_FRF_MOTO | SSI_CR0_DSS_8;
 	SSI0_CR1_R = SSI_CR1_SSE;
