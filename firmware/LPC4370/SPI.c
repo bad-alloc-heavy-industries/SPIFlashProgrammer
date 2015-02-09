d654c737321fe75d0a6579e9429b61cc5afd1ac6
dx-mon <dx-mon@users.sourceforge.net>
2015-02-09 14:55:51 +0000

    Implemented some more initialisation code for the SPI functionality

--- firmware/LPC4370/SPI.c
+++ firmware/LPC4370/SPI.c
@@ -19,10 +19,28 @@
 #include <LPC4370.h>
 #include "SPI.h"
 
+/*
+ * Func1 for pins 3, 6, 7, 8, uses block SPI
+ * Func3 for pins 3, 4, 5, 6, 7, 8 uses block SPIFI
+ */
+
 void spiInit()
 {
-	/* Initialise SPIFI (P3_{3..8}) to access SPI Flash */
+	/*
+	 * Set the port to digital mode, configure for open-collector based IO and switch into SPI mode
+	 * This sets the TX/!CS/CLK pins to outputs, RX as input
+	 */
+	SCU->SFS_Port3[3] = SCU_SFS_MODE_1;
+	SCU->SFS_Port3[6] = SCU_SFS_MODE_1 | SCU_SFS_DPU | SCU_SFS_EIB;
+	SCU->SFS_Port3[7] = SCU_SFS_MODE_1;
+	SCU->SFS_Port3[8] = SCU_SFS_MODE_0; /* !CS needs to be under our control so do not send it into the SPI peripheral */
 	SPI->CR = 0;
+
+	/* Set !CS as GPIO output */
+	GPIO_PORT3_DIR |= 0x00000100;
+	/* And send !CS high */
+	GPIO_PORT3_SET = 0x00000100;
+
 	/* Set Freescale SPI, SPO = 1, SPH = 1 */
 	SPI->CR = SPI_CR_SPO | SPI_CR_SPH | SPI_CR_MASTER | SPI_CR_DSS_EN | SPI_CR_DSS_8;
 	/* Scale the clock by 8 (the datasheet minimum) to make it 4MHz */
