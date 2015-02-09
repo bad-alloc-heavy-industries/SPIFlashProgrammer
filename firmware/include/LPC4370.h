96bc5d7fa094127001522a2d4fa6e0fd790d9a48
dx-mon <dx-mon@users.sourceforge.net>
2015-02-09 12:38:02 +0000

    Begun defining the registers present in the LPC4370 and building up a suitable header file for it

---
+++ firmware/include/LPC4370.h
@@ -0,0 +1,119 @@
+#ifndef __LPC4370_H__
+#define __LPC4370_H__
+
+#include <stdint.h>
+
+typedef struct
+{
+	volatile uint32_t CR;
+	volatile uint32_t SR;
+	volatile uint32_t DR;
+	volatile uint32_t CCR; /* aka CPSR */
+	volatile uint32_t reserved[3];
+	volatile uint32_t INT;
+} lpcSPI_t;
+
+#define GPIO_PORT0_BYTES	((volatile uint8_t *)0x400F4000)
+#define GPIO_PORT0_WORDS	((volatile uint32_t *)0x400F5000)
+#define GPIO_PORT0_DIR		*((volatile uint32_t *)0x400F6000)
+#define GPIO_PORT0_MASK		*((volatile uint32_t *)0x400F6080)
+#define GPIO_PORT0_PIN		*((volatile uint32_t *)0x400F6100)
+#define GPIO_PORT0_MPIN		*((volatile uint32_t *)0x400F6180)
+#define GPIO_PORT0_SET		*((volatile uint32_t *)0x400F6200)
+#define GPIO_PORT0_CLR		*((volatile uint32_t *)0x400F6280)
+#define GPIO_PORT0_TGL		*((volatile uint32_t *)0x400F6300)
+
+#define GPIO_PORT1_BYTES	((volatile uint8_t *)0x400F4020)
+#define GPIO_PORT1_WORDS	((volatile uint32_t *)0x400F5080)
+#define GPIO_PORT1_DIR		*((volatile uint32_t *)0x400F6004)
+#define GPIO_PORT1_MASK		*((volatile uint32_t *)0x400F6084)
+#define GPIO_PORT1_PIN		*((volatile uint32_t *)0x400F6104)
+#define GPIO_PORT1_MPIN		*((volatile uint32_t *)0x400F6184)
+#define GPIO_PORT1_SET		*((volatile uint32_t *)0x400F6204)
+#define GPIO_PORT1_CLR		*((volatile uint32_t *)0x400F6284)
+#define GPIO_PORT1_TGL		*((volatile uint32_t *)0x400F6304)
+
+#define GPIO_PORT2_BYTES	((volatile uint8_t *)0x400F4040)
+#define GPIO_PORT2_WORDS	((volatile uint32_t *)0x400F5100)
+#define GPIO_PORT2_DIR		*((volatile uint32_t *)0x400F6008
+#define GPIO_PORT2_MASK		*((volatile uint32_t *)0x400F6088)
+#define GPIO_PORT2_PIN		*((volatile uint32_t *)0x400F6108)
+#define GPIO_PORT2_MPIN		*((volatile uint32_t *)0x400F6188)
+#define GPIO_PORT2_SET		*((volatile uint32_t *)0x400F6208)
+#define GPIO_PORT2_CLR		*((volatile uint32_t *)0x400F6288)
+#define GPIO_PORT2_TGL		*((volatile uint32_t *)0x400F6308)
+
+#define GPIO_PORT3_BYTES	((volatile uint8_t *)0x400F4060)
+#define GPIO_PORT3_WORDS	((volatile uint32_t *)0x400F5180)
+#define GPIO_PORT3_DIR		*((volatile uint32_t *)0x400F600C)
+#define GPIO_PORT3_MASK		*((volatile uint32_t *)0x400F608C)
+#define GPIO_PORT3_PIN		*((volatile uint32_t *)0x400F610C)
+#define GPIO_PORT3_MPIN		*((volatile uint32_t *)0x400F618C)
+#define GPIO_PORT3_SET		*((volatile uint32_t *)0x400F620C)
+#define GPIO_PORT3_CLR		*((volatile uint32_t *)0x400F628C)
+#define GPIO_PORT3_TGL		*((volatile uint32_t *)0x400F630C)
+
+#define GPIO_PORT4_BYTES	((volatile uint8_t *)0x400F4080)
+#define GPIO_PORT4_WORDS	((volatile uint32_t *)0x400F5200)
+#define GPIO_PORT4_DIR		*((volatile uint32_t *)0x400F6010)
+#define GPIO_PORT4_MASK		*((volatile uint32_t *)0x400F6090)
+#define GPIO_PORT4_PIN		*((volatile uint32_t *)0x400F6110)
+#define GPIO_PORT4_MPIN		*((volatile uint32_t *)0x400F6190)
+#define GPIO_PORT4_SET		*((volatile uint32_t *)0x400F6210)
+#define GPIO_PORT4_CLR		*((volatile uint32_t *)0x400F6290)
+#define GPIO_PORT4_TGL		*((volatile uint32_t *)0x400F6310)
+
+#define GPIO_PORT5_BYTES	((volatile uint8_t *)0x400F40A0)
+#define GPIO_PORT5_WORDS	((volatile uint32_t *)0x400F5280)
+#define GPIO_PORT5_DIR		*((volatile uint32_t *)0x400F6014)
+#define GPIO_PORT5_MASK		*((volatile uint32_t *)0x400F6094)
+#define GPIO_PORT5_PIN		*((volatile uint32_t *)0x400F6114)
+#define GPIO_PORT5_MPIN		*((volatile uint32_t *)0x400F6194)
+#define GPIO_PORT5_SET		*((volatile uint32_t *)0x400F6214)
+#define GPIO_PORT5_CLR		*((volatile uint32_t *)0x400F6294)
+#define GPIO_PORT5_TGL		*((volatile uint32_t *)0x400F6314)
+
+#define GPIO_PORT6_BYTES	((volatile uint8_t *)0x400F40C0)
+#define GPIO_PORT6_WORDS	((volatile uint32_t *)0x400F5300)
+#define GPIO_PORT6_DIR		*((volatile uint32_t *)0x400F6018)
+#define GPIO_PORT6_MASK		*((volatile uint32_t *)0x400F6098)
+#define GPIO_PORT6_PIN		*((volatile uint32_t *)0x400F6118)
+#define GPIO_PORT6_MPIN		*((volatile uint32_t *)0x400F6198)
+#define GPIO_PORT6_SET		*((volatile uint32_t *)0x400F6218)
+#define GPIO_PORT6_CLR		*((volatile uint32_t *)0x400F6298)
+#define GPIO_PORT6_TGL		*((volatile uint32_t *)0x400F6318)
+
+#define GPIO_PORT7_BYTES	((volatile uint8_t *)0x400F40E0)
+#define GPIO_PORT7_WORDS	((volatile uint32_t *)0x400F5380)
+#define GPIO_PORT7_DIR		*((volatile uint32_t *)0x400F601C)
+#define GPIO_PORT7_MASK		*((volatile uint32_t *)0x400F609C)
+#define GPIO_PORT7_PIN		*((volatile uint32_t *)0x400F611C)
+#define GPIO_PORT7_MPIN		*((volatile uint32_t *)0x400F619C)
+#define GPIO_PORT7_SET		*((volatile uint32_t *)0x400F621C)
+#define GPIO_PORT7_CLR		*((volatile uint32_t *)0x400F629C)
+#define GPIO_PORT7_TGL		*((volatile uint32_t *)0x400F631C)
+
+static lpcSPI_t *SPI = (lpcSPI_t *)0x40100000;
+
+#define SPI_CR_MASTER			0x00000020
+#define SPI_CR_SPO				0x00000010
+#define SPI_CR_SPH				0x00000008
+#define SPI_CR_DSS_EN			0x00000004
+#define SPI_CR_DSS_8			0x00000800 /* 8-bit data */
+#define SPI_CR_DSS_9			0x00000900 /* 9-bit data */
+#define SPI_CR_DSS_10			0x00000A00 /* 10-bit data */
+#define SPI_CR_DSS_11			0x00000B00 /* 11-bit data */
+#define SPI_CR_DSS_12			0x00000C00 /* 12-bit data */
+#define SPI_CR_DSS_13			0x00000D00 /* 13-bit data */
+#define SPI_CR_DSS_14			0x00000E00 /* 14-bit data */
+#define SPI_CR_DSS_15			0x00000F00 /* 15-bit data */
+#define SPI_CR_DSS_16			0x00000000 /* 16-bit data */
+
+#define SPI_SR_ABRT				0x00000008 /* SPI slave abort */
+#define SPI_SR_MODF				0x00000010 /* SPI mode fault */
+#define SPI_SR_ROVR				0x00000020 /* SPI read overrun */
+#define SPI_SR_WCOL				0x00000040 /* SPI write collision */
+#define SPI_SR_SPIF				0x00000080 /* SPI transfer completed */
+
+#endif /*__LPC4370_H__*/
+
