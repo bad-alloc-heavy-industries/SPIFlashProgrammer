49a6db9588cdad25546ae4b61570791ffd687d2d
dx-mon <dx-mon@users.sourceforge.net>
2015-02-26 12:22:41 +0000

    Implemented the device descriptor but have not yet filled in a suitable VID/PID pair

--- firmware/LPC4370/USBRequests.c
+++ firmware/LPC4370/USBRequests.c
@@ -21,10 +21,31 @@
 #include "USBTypes.h"
 #include "USBRequests.h"
 
+#define USB_VID					0x0
+#define USB_PID					0x0
+
 #define USB_NUM_STRING_DESC		4
 
 #define USB_EPDIR_IN			0x80
 #define USB_EPDIR_OUT			0x00
+
+const usbDeviceDescriptor_t usbDeviceDesc =
+{
+	sizeof(usbDeviceDescriptor_t),
+	USB_DESCRIPTOR_DEVICE,
+	0x0200, /* this is 2.00 in USB's BCD format */
+	USB_CLASS_COMMS,
+	USB_SUBCLASS_NONE,
+	USB_PROTOCOL_NONE,
+	USB_EP0_SETUP_LEN,
+	USB_VID,
+	USB_PID,
+	0x0001, /* BCD encoded device version */
+	0x01, /* Manufacturer string index */
+	0x02, /* Product string index */
+	0x00, /* Temporarily do not support a serial number string */
+	USB_NUM_CONFIG_DESC /* One configuration only */
+};
 
 const struct
 {
