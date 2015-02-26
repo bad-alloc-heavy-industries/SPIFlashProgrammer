89e6bb01a9114d2eeeb1b9edb3ff07599a4a53f0
dx-mon <dx-mon@users.sourceforge.net>
2015-02-26 12:20:09 +0000

    Implemented the string descriptors the LPC4370 will use

--- firmware/LPC4370/USBRequests.c
+++ firmware/LPC4370/USBRequests.c
@@ -21,8 +21,80 @@
 #include "USBTypes.h"
 #include "USBRequests.h"
 
+#define USB_NUM_STRING_DESC		4
+
 #define USB_EPDIR_IN			0x80
 #define USB_EPDIR_OUT			0x00
+
+const struct
+{
+	usbStringDescBase_t header;
+	uint16_t ids[1];
+} usbStringLangIDs =
+{
+	{
+		sizeof(usbStringLangIDs),
+		USB_DESCRIPTOR_STRING
+	},
+	{ 0x0409 } /* This encodes the ID code for US English so we work nicely with windows */
+};
+
+const struct
+{
+	usbStringDescBase_t header;
+	uint16_t strMfr[11];
+} usbStringMfr =
+{
+	{
+		sizeof(usbStringMfr),
+		USB_DESCRIPTOR_STRING
+	},
+	{
+		/* Manufacturer string is "Rachel Mant" */
+		'R', 'a', 'c', 'h', 'e', 'l', ' ', 'M', 'a', 'n', 't'
+	}
+};
+
+const struct
+{
+	usbStringDescBase_t header;
+	uint16_t strProduct[18];
+} usbStringProduct =
+{
+	{
+		sizeof(usbStringProduct),
+		USB_DESCRIPTOR_STRING
+	},
+	{
+		/* Product string is "SPIFlashProgrammer" */
+		'S', 'P', 'I', 'F', 'l', 'a', 's', 'h', 'P', 'r', 'o', 'g',
+		'r', 'a', 'm', 'm', 'e', 'r'
+	}
+};
+
+const struct
+{
+	usbStringDescBase_t header;
+	uint16_t strUART[8];
+} usbStringUART =
+{
+	{
+		sizeof(usbStringUART),
+		USB_DESCRIPTOR_STRING
+	},
+	{
+		/* Primary configuration string is "USB UART" */
+		'U', 'S', 'B', ' ', 'U', 'A', 'R', 'T'
+	}
+};
+
+const usbStringDescBase_t *usbStrings[USB_NUM_STRING_DESC] =
+{
+	&usbStringLangIDs.header,
+	&usbStringMfr.header,
+	&usbStringProduct.header,
+	&usbStringUART.header
+};
 
 void usbRequestGetDescriptor()
 {
