086d46c7cbf15ff2ee3216bbcc8cd18aebc1e257
dx-mon <dx-mon@users.sourceforge.net>
2015-02-26 12:09:50 +0000

    Added stubs for all the USB EP0 requests and integrated the new files into the build system

---
+++ firmware/LPC4370/USBRequests.h
@@ -0,0 +1,30 @@
+/*
+ * This file is part of SPI Flash Programmer (SPIFP)
+ * Copyright © 2014 Rachel Mant (dx-mon@users.sourceforge.net)
+ *
+ * SPIFP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SPIFP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef USBREQUESTS_H
+#define	USBREQUESTS_H
+
+extern void usbRequestGetDescriptor();
+extern void usbRequestSetConfiguration();
+extern void usbRequestGetStatus();
+extern void usbRequestDoFeature();
+
+extern volatile usbDeviceState usbState;
+extern volatile uint8_t usbActiveConfig;
+
+#endif	/* USBREQUESTS_H */
