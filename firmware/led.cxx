c2dd91a24e61be5c1a6acae3793b7b3902051746
dx-mon <dx-mon@users.sourceforge.net>
2020-11-10 21:57:01 +0000

    Removed the ability for the peripheral structure overlays to be changed or need explicit dereferening

--- firmware/led.cxx
+++ firmware/led.cxx
@@ -5,23 +5,23 @@
 void ledInit() noexcept
 {
 	// Dwitch over to port D's AHB apeture
-	sysCtrl->gpioAHBCtrl |= sysCtrlGPIOAHBCtrlPortD;
+	sysCtrl.gpioAHBCtrl |= sysCtrlGPIOAHBCtrlPortD;
 	// Enable port D
-	sysCtrl->runClockGateCtrlGPIO |= sysCtrlRunClockGateCtrlGPIOD;
+	sysCtrl.runClockGateCtrlGPIO |= sysCtrlRunClockGateCtrlGPIOD;
 
 	// Wait for the port to come online
-	while (!(sysCtrl->periphReadyGPIO & sysCtrlPeriphReadyGPIOD))
+	while (!(sysCtrl.periphReadyGPIO & sysCtrlPeriphReadyGPIOD))
 		continue;
 
 	// Port D is protected, so enable changing it to digital GPIO
-	gpioD->lock = gpioLockKey;
-	gpioD->commit |= 0x01;
-	gpioD->lock = 0;
+	gpioD.lock = gpioLockKey;
+	gpioD.commit |= 0x01;
+	gpioD.lock = 0;
 	// Set the RGB LED pins as digital mode outputs
-	gpioD->den |= 0x07;
-	gpioD->afSel &= ~0x07;
-	gpioD->dir = 0x07;
+	gpioD.den |= 0x07;
+	gpioD.afSel &= ~0x07;
+	gpioD.dir = 0x07;
 
 	// Turn the LED purple.
-	gpioD->dataBits[0x07] = 0x06;
+	gpioD.dataBits[0x07] = 0x06;
 }
