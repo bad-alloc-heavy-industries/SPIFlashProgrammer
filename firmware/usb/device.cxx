66467760f023ba0d33305bfff57bf42c9037adab
dx-mon <dx-mon@users.sourceforge.net>
2020-11-17 02:28:28 +0000

    Begun implementing handling for setup packets and the meat of usbHandleStandardRequest()

--- firmware/usb/device.cxx
+++ firmware/usb/device.cxx
@@ -2,8 +2,37 @@
 #include "usb/types.hxx"
 #include "usb/device.hxx"
 
+using namespace usbTypes;
+
+answer_t usbHandleStandardRequest() noexcept
+{
+	const auto &packet{*reinterpret_cast<setupPacket_t *>(usbStatusOutEP[0].usbBuffer)};
+	const auto &epStatus{usbStatusInEP[0]};
+
+	switch (packet.request)
+	{
+	}
+
+	return {response_t::unhandled, nullptr, 0};
+}
+
 void usbHandleCtrlEPSetup()
 {
+	// Set up EP0 state for a reply of some kind
+	usbCtrlState = ctrlState_t::wait;
+	usbStatusInEP[0].needsArming(false);
+	usbStatusInEP[0].stall(false);
+	usbStatusInEP[0].transferCount = 0;
+	usbStatusOutEP[0].needsArming(false);
+	usbStatusOutEP[0].stall(false);
+	usbStatusOutEP[0].transferCount = 0;
+
+	const auto &[response, data, size] = usbHandleStandardRequest();
+
+	usbStatusInEP[0].stall(response == response_t::stall || response == response_t::unhandled);
+	usbStatusInEP[0].needsArming(response == response_t::data || response == response_t::zeroLength);
+	usbStatusInEP[0].transferCount = size;
+	usbServiceCtrlEPComplete();
 }
 
 void usbHandleCtrlEPOut()
@@ -18,9 +47,9 @@ void usbServiceCtrlEP() noexcept
 {
 	if (usbPacket.endpoint() != 0)
 		return;
-	if (usbPacket.dir() == usbTypes::endpointDir_t::controllerOut)
+	if (usbPacket.dir() == endpointDir_t::controllerOut)
 	{
-		if (usbCtrlState == usbTypes::ctrlState_t::idle)
+		if (usbCtrlState == ctrlState_t::idle)
 			usbHandleCtrlEPSetup();
 		else
 			usbHandleCtrlEPOut();
--- firmware/usb/device.hxx
+++ firmware/usb/device.hxx
@@ -12,6 +12,45 @@ extern std::array<usbTypes::usbEPStatus_t, usbTypes::endpointCount> usbStatusInE
 extern std::array<usbTypes::usbEPStatus_t, usbTypes::endpointCount> usbStatusOutEP;
 
 extern void usbServiceCtrlEP() noexcept;
-extern usbTypes::answer_t usbHandleStandardRequest() noexcept;
+
+namespace usbTypes
+{
+	enum class request_t : uint8_t
+	{
+		typeStandard = 0x00,
+		typeClass = 0x20U,
+		typeVendor = 0x40U
+	};
+
+	enum class recipient_t : uint8_t
+	{
+		device = 0,
+		interface = 1,
+		endpoint = 2,
+		other = 3
+	};
+
+	struct setupPacket_t
+	{
+	private:
+		struct requestType_t
+		{
+		private:
+			uint8_t value{};
+
+		public:
+			requestType_t() = default;
+
+			[[nodiscard]] recipient_t recipient() const noexcept
+				{ return static_cast<recipient_t>(value & 0x1FU; }
+			[[nodiscard]] request_t type() const noexcept
+				{ return static_cast<request_t>(value & 0x60U); }
+			[[nodiscard]] endpointDir_t dir() const noexcept
+				{ return static_cast<endpointDir_t>(value & 0x80U); }
+		};
+
+		setupPacket_t() = default;
+	};
+} // namespace usb
 
 #endif /*USB_DEVICE__HXX*/
