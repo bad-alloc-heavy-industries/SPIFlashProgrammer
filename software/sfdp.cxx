dfd9a5f663a5c17dd5819c552ab199eb459d4fcd
dragonmux <git@dragonmux.network>
2023-02-06 08:02:57 +0000

    Implemented handling of the SFDP parameter table headers

--- software/sfdp.cxx
+++ software/sfdp.cxx
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include <string_view>
 #include <substrate/console>
+#include <substrate/index_sequence>
 #include "sfdp.hxx"
 #include "sfdpInternal.hxx"
 #include "usbProtocol.hxx"
@@ -8,6 +9,7 @@
 using namespace std::literals::string_view_literals;
 using substrate::console;
 using substrate::asHex_t;
+using substrate::indexSequence_t;
 using namespace flashProto;
 
 namespace sfdp
@@ -16,7 +18,7 @@ namespace sfdp
 	constexpr static uint32_t tableHeaderAddress{sizeof(sfdpHeader_t)};
 
 	constexpr static std::array<char, 4> sfdpMagic{{'S', 'F', 'D', 'P'}};
-	constexpr static uint16_t basicSPIParameterTable{0xFF00};
+	constexpr static uint16_t basicSPIParameterTable{0xFF00U};
 
 	[[nodiscard]] static bool sfdpRead(const usbDeviceHandle_t &device, const usbDataSource_t &dataSource,
 		const uint32_t address, void *const buffer, const size_t bufferLen)
@@ -33,17 +35,26 @@ namespace sfdp
 	static void displayHeader(const sfdpHeader_t &header)
 	{
 		console.info("SFDP Header:"sv);
-		console.writeln("-> magic '"sv, header.magic, "'"sv);
-		console.writeln("-> version "sv, header.versionMajor, '.', header.versionMinor);
-		console.writeln("-> "sv, header.parameterHeadersCount(), " parameter headers"sv);
-		console.writeln("-> access protocol "sv, asHex_t<2, '0'>{uint8_t(header.accessProtocol)});
+		console.info("-> magic '"sv, header.magic, "'"sv);
+		console.info("-> version "sv, header.versionMajor, '.', header.versionMinor);
+		console.info("-> "sv, header.parameterHeadersCount(), " parameter headers"sv);
+		console.info("-> access protocol "sv, asHex_t<2, '0'>{uint8_t(header.accessProtocol)});
+	}
+
+	static void displayTableHeader(const parameterTableHeader_t &header, const size_t index)
+	{
+		console.info("Parameter table header "sv, index, ":"sv);
+		console.info("-> type "sv, asHex_t<4, '0'>{header.jedecParameterID()});
+		console.info("-> version "sv, header.versionMajor, '.', header.versionMinor);
+		console.info("-> table is "sv, header.tableLength(), " bytes long"sv);
+		console.info("-> table SFDP address: "sv, uint32_t{header.tableAddress});
 	}
 
 	bool readAndDisplay(const usbDeviceHandle_t &device, const usbDataSource_t dataSource)
 	{
 		console.info("Reading SFDP data for device"sv);
 		sfdpHeader_t header{};
-		if (sfdpRead(device, dataSource, sfdpHeaderAddress, header))
+		if (!sfdpRead(device, dataSource, sfdpHeaderAddress, header))
 			return false;
 		if (header.magic != sfdpMagic)
 		{
@@ -54,6 +65,13 @@ namespace sfdp
 		}
 		displayHeader(header);
 
+		for (const auto idx : indexSequence_t{header.parameterHeadersCount()})
+		{
+			parameterTableHeader_t tableHeader{};
+			if (!sfdpRead(device, dataSource, tableHeaderAddress + (sizeof(parameterTableHeader_t) * idx), tableHeader))
+				return false;
+			displayTableHeader(tableHeader, idx + 1U);
+		}
 		return true;
 	}
 } // namespace sfdp
